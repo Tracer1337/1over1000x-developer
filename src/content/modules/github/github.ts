3d80dc3dda32c142f02d1b9653991b2abfc06f2f
Tracer1337 <merlin.moelter@gmail.com>
2024-06-26 11:49:30 +0200

    Remove github dashboard mod

--- src/content/modules/github/github.ts
+++ src/content/modules/github/github.ts
@@ -1,14 +1,9 @@
 import { registerRouteHandler } from 'shared/bridge';
 import { setupFileViewer } from './file';
-import { setupDashboard } from './dashboard';
 
 export function setup() {
   registerRouteHandler({
     path: /https:\/\/github.com\/.*drawio\/?/,
     callback: setupFileViewer,
-  });
-  registerRouteHandler({
-    path: /https:\/\/github\.com\/(dashboard)?$/,
-    callback: setupDashboard,
   });
 }
